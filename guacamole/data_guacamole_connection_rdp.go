b5991a38f561e7191abf1228b064330dea3ba304
kbcz1989 <fcrs00@bk.ru>
2022-07-10 14:33:47 +0200

    Set parameters as Optional in data sources, set username as Computed in vnc data source

--- guacamole/data_guacamole_connection_rdp.go
+++ guacamole/data_guacamole_connection_rdp.go
@@ -91,7 +91,7 @@ func dataSourceConnectionRDP() *schema.Resource {
 			"parameters": {
 				Type:        schema.TypeList,
 				Description: "Guacamole connection parameters",
-				Required:    true,
+				Optional:    true,
 				MaxItems:    1,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
